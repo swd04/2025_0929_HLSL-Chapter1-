b0d71cddc9a3f975c20967490dec9116e3b3391a
GAME\242J009 <zazk2714@gmail.com>
2025-11-11 10:40:20 +0900

    5-4まで終りました

--- Sample_05_04/Sample_05_04/Assets/shader/sample.fx
+++ Sample_05_04/Sample_05_04/Assets/shader/sample.fx
@@ -4,19 +4,19 @@
 // 頂点シェーダーへの入力
 struct SVSIn
 {
-    float4 pos      : POSITION;
-    float3 normal   : NORMAL;
-    float2 uv       : TEXCOORD0;
+	float4 pos      : POSITION;
+	float3 normal   : NORMAL;
+	float2 uv       : TEXCOORD0;
 };
 
 // ピクセルシェーダーへの入力
 struct SPSIn
 {
-    float4 pos          : SV_POSITION;
-    float3 normal       : NORMAL;
-    float2 uv           : TEXCOORD0;
-    float3 worldPos     : TEXCOORD1;
-    float3 normalInView : TEXCOORD2; // カメラ空間の法線
+	float4 pos          : SV_POSITION;
+	float3 normal       : NORMAL;
+	float2 uv           : TEXCOORD0;
+	float3 worldPos     : TEXCOORD1;
+	float3 normalInView : TEXCOORD2; // カメラ空間の法線
 };
 
 ///////////////////////////////////////////
@@ -25,22 +25,24 @@ struct SPSIn
 // モデル用の定数バッファー
 cbuffer ModelCb : register(b0)
 {
-    float4x4 mWorld;
-    float4x4 mView;
-    float4x4 mProj;
+	float4x4 mWorld;
+	float4x4 mView;
+	float4x4 mProj;
 };
 
 // ライトデータにアクセスするための定数バッファーを用意する
 cbuffer DirectionLightCb : register(b1)
 {
-    // ディレクションライト用のデータ
-    float3 dirDirection;    // ライトの方向
-    float3 dirColor;        // ライトのカラー
-    float3 eyePos;          // 視点の位置
-    float3 ambientLight;    // アンビエントライト
+	// ディレクションライト用のデータ
+	float3 dirDirection;    // ライトの方向
+	float3 dirColor;        // ライトのカラー
+	float3 eyePos;          // 視点の位置
+	float3 ambientLight;    // アンビエントライト
 
-    // step-3 半球ライトのデータにアクセスするための変数を追加
-
+	// step-3 半球ライトのデータにアクセスするための変数を追加
+	float3 groundColor;     // 照り返しのライト
+	float3 skyColor;        // 天球ライト
+	float3 groundNormal;    // 地面の法線
 };
 
 ///////////////////////////////////////////
@@ -66,18 +68,18 @@ sampler g_sampler : register(s0);
 /// </summary>
 SPSIn VSMain(SVSIn vsIn, uniform bool hasSkin)
 {
-    SPSIn psIn;
+	SPSIn psIn;
 
-    psIn.pos = mul(mWorld, vsIn.pos);   // モデルの頂点をワールド座標系に変換
-    psIn.worldPos = psIn.pos;
-    psIn.pos = mul(mView, psIn.pos);    // ワールド座標系からカメラ座標系に変換
-    psIn.pos = mul(mProj, psIn.pos);    // カメラ座標系からスクリーン座標系に変換
+	psIn.pos = mul(mWorld, vsIn.pos);   // モデルの頂点をワールド座標系に変換
+	psIn.worldPos = psIn.pos;
+	psIn.pos = mul(mView, psIn.pos);    // ワールド座標系からカメラ座標系に変換
+	psIn.pos = mul(mProj, psIn.pos);    // カメラ座標系からスクリーン座標系に変換
 
-    // 頂点法線をピクセルシェーダーに渡す
-    psIn.normal = mul(mWorld, vsIn.normal);      // 法線を回転させる
-    psIn.uv = vsIn.uv;
-    psIn.normalInView = mul(mView,psIn.normal); // カメラ空間の法線を求める
-    return psIn;
+	// 頂点法線をピクセルシェーダーに渡す
+	psIn.normal = mul(mWorld, vsIn.normal);      // 法線を回転させる
+	psIn.uv = vsIn.uv;
+	psIn.normalInView = mul(mView, psIn.normal); // カメラ空間の法線を求める
+	return psIn;
 }
 
 /// <summary>
@@ -85,21 +87,30 @@ SPSIn VSMain(SVSIn vsIn, uniform bool hasSkin)
 /// </summary>
 float4 PSMain(SPSIn psIn) : SV_Target0
 {
-    // ディレクションライトによるライティングを計算する
-    float3 directionLig = CalcLigFromDirectionLight(psIn);
+	// ディレクションライトによるライティングを計算する
+	float3 directionLig = CalcLigFromDirectionLight(psIn);
 
-    // step-4 半球ライトを計算する
+	// step-4 半球ライトを計算する
+	// サーフェイスの法線と地面の法線との内積を計算する
+	float t = dot(psIn.normal, groundNormal);
 
-    // 各種ライトの反射光を足し算して最終的な反射光を求める
-    float3 finalLig = directionLig + ambientLight;
+	// 内積の結果を0～1の範囲に変換する
+	t = (t + 1.0f) / 2.0f;
 
-    // step-5 半球ライトを最終的な反射光に加算する
-    float4 finalColor = g_texture.Sample(g_sampler, psIn.uv);
+	// 地面色と天球色を補完率tで線形補完する
+	float3 hemiLight = lerp(groundColor, skyColor, t);
 
-    // テクスチャカラーに求めた光を乗算して最終出力カラーを求める
-    finalColor.xyz *= finalLig;
+	// 各種ライトの反射光を足し算して最終的な反射光を求める
+	float3 finalLig = directionLig + ambientLight;
 
-    return finalColor;
+	// step-5 半球ライトを最終的な反射光に加算する
+	finalLig += hemiLight;
+	float4 finalColor = g_texture.Sample(g_sampler, psIn.uv);
+
+	// テクスチャカラーに求めた光を乗算して最終出力カラーを求める
+	finalColor.xyz *= finalLig;
+
+	return finalColor;
 }
 
 /// <summary>
@@ -107,14 +118,14 @@ float4 PSMain(SPSIn psIn) : SV_Target0
 /// </summary>
 float3 CalcLambertDiffuse(float3 lightDirection, float3 lightColor, float3 normal)
 {
-    // ピクセルの法線とライトの方向の内積を計算する
-    float t = dot(normal, lightDirection) * -1.0f;
+	// ピクセルの法線とライトの方向の内積を計算する
+	float t = dot(normal, lightDirection) * -1.0f;
 
-    // 内積の値を0以上の値にする
-    t = max(0.0f, t);
+	// 内積の値を0以上の値にする
+	t = max(0.0f, t);
 
-    // 拡散反射光を計算する
-    return lightColor * t;
+	// 拡散反射光を計算する
+	return lightColor * t;
 }
 
 /// <summary>
@@ -122,24 +133,24 @@ float3 CalcLambertDiffuse(float3 lightDirection, float3 lightColor, float3 norma
 /// </summary>
 float3 CalcPhongSpecular(float3 lightDirection, float3 lightColor, float3 worldPos, float3 normal)
 {
-    // 反射ベクトルを求める
-    float3 refVec = reflect(lightDirection, normal);
+	// 反射ベクトルを求める
+	float3 refVec = reflect(lightDirection, normal);
 
-    // 光が当たったサーフェイスから視点に伸びるベクトルを求める
-    float3 toEye = eyePos - worldPos;
-    toEye = normalize(toEye);
+	// 光が当たったサーフェイスから視点に伸びるベクトルを求める
+	float3 toEye = eyePos - worldPos;
+	toEye = normalize(toEye);
 
-    // 鏡面反射の強さを求める
-    float t = dot(refVec, toEye);
+	// 鏡面反射の強さを求める
+	float t = dot(refVec, toEye);
 
-    // 鏡面反射の強さを0以上の数値にする
-    t = max(0.0f, t);
+	// 鏡面反射の強さを0以上の数値にする
+	t = max(0.0f, t);
 
-    // 鏡面反射の強さを絞る
-    t = pow(t, 5.0f);
+	// 鏡面反射の強さを絞る
+	t = pow(t, 5.0f);
 
-    // 鏡面反射光を求める
-    return lightColor * t;
+	// 鏡面反射光を求める
+	return lightColor * t;
 }
 
 /// <summary>
@@ -148,10 +159,10 @@ float3 CalcPhongSpecular(float3 lightDirection, float3 lightColor, float3 worldP
 /// <param name="psIn">ピクセルシェーダーからの入力。</param>
 float3 CalcLigFromDirectionLight(SPSIn psIn)
 {
-    // ディレクションライトによるLambert拡散反射光を計算する
-    float3 diffDirection = CalcLambertDiffuse(dirDirection, dirColor, psIn.normal);
+	// ディレクションライトによるLambert拡散反射光を計算する
+	float3 diffDirection = CalcLambertDiffuse(dirDirection, dirColor, psIn.normal);
 
-    // ディレクションライトによるPhong鏡面反射光を計算する
-    float3 specDirection = CalcPhongSpecular(dirDirection, dirColor, psIn.worldPos, psIn.normal);
-    return diffDirection + specDirection;
+	// ディレクションライトによるPhong鏡面反射光を計算する
+	float3 specDirection = CalcPhongSpecular(dirDirection, dirColor, psIn.worldPos, psIn.normal);
+	return diffDirection + specDirection;
 }
--- Sample_05_04/Sample_05_04/main.cpp
+++ Sample_05_04/Sample_05_04/main.cpp
@@ -6,18 +6,23 @@
 /// </summary>
 struct Light
 {
-    // ディレクションライト用のメンバ
-    Vector3 dirDirection;   // ライトの方向
-    float pad0;
-    Vector3 dirColor;       // ライトのカラー
-    float pad1;
+	// ディレクションライト用のメンバ
+	Vector3 dirDirection;   // ライトの方向
+	float pad0;
+	Vector3 dirColor;       // ライトのカラー
+	float pad1;
 
-    Vector3 eyePos;         // 視点の位置
-    float pad2;
-    Vector3 ambientLight;   // アンビエントライト
-    float pad3;
+	Vector3 eyePos;         // 視点の位置
+	float pad2;
+	Vector3 ambientLight;   // アンビエントライト
+	float pad3;
 
-    // step-1 地面色と天球色、地面の法線を追加する
+	// step-1 地面色と天球色、地面の法線を追加する
+	Vector3 groundColor;  //地面色
+	float pad4;
+	Vector3 skyColor;     //天球色
+	float pad5;
+	Vector3 groundNormal; //地面の法線
 };
 
 //////////////////////////////////////
@@ -32,66 +37,69 @@ void InitAmbientLight(Light& light);
 // ウィンドウプログラムのメイン関数
 ///////////////////////////////////////////////////////////////////
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
-                    LPWSTR lpCmdLine, int nCmdShow)
+	LPWSTR lpCmdLine, int nCmdShow)
 {
-    // ゲームの初期化
-    InitGame(hInstance, hPrevInstance, lpCmdLine, nCmdShow, TEXT("Game"));
+	// ゲームの初期化
+	InitGame(hInstance, hPrevInstance, lpCmdLine, nCmdShow, TEXT("Game"));
 
-    g_camera3D->SetPosition({ 0.0f, 50.0f, 200.0f });
-    g_camera3D->SetTarget({ 0.0f, 50.0f, 0.0f });
+	g_camera3D->SetPosition({ 0.0f, 50.0f, 200.0f });
+	g_camera3D->SetTarget({ 0.0f, 50.0f, 0.0f });
 
-    //////////////////////////////////////
-    // ここから初期化を行うコードを記述する
-    //////////////////////////////////////
-    // ライトのデータを作成する
-    Light light;
+	//////////////////////////////////////
+	// ここから初期化を行うコードを記述する
+	//////////////////////////////////////
+	// ライトのデータを作成する
+	Light light;
 
-    // ディレクションライトを初期化する
-    InitDirectionLight(light);
+	// ディレクションライトを初期化する
+	InitDirectionLight(light);
 
-    // アンビエントライトを初期化する
-    InitAmbientLight(light);
+	// アンビエントライトを初期化する
+	InitAmbientLight(light);
 
-    // step-2 地面色、天球色、地面の法線のデータを設定する
+	// step-2 地面色、天球色、地面の法線のデータを設定する
+	light.groundColor = { 0.7f,0.5f,0.3f };  //地面色を設定
+	light.skyColor = { 0.15f,0.7f,0.95f };   //天球色を設定
+	light.groundNormal = { 0.0f,1.0f,0.0f }; //地面の法線を設定
 
-    // モデルを初期化する
-    // モデルを初期化するための情報を構築する
-    Model lightModel, bgModel, teapotModel;
-    InitModel(bgModel, teapotModel, lightModel , light);
+	// モデルを初期化する
+	// モデルを初期化するための情報を構築する
+	Model lightModel, bgModel, teapotModel;
+	InitModel(bgModel, teapotModel, lightModel, light);
 
-    //////////////////////////////////////
-    // 初期化を行うコードを書くのはここまで！！！
-    //////////////////////////////////////
-    auto& renderContext = g_graphicsEngine->GetRenderContext();
+	//////////////////////////////////////
+	// 初期化を行うコードを書くのはここまで！！！
+	//////////////////////////////////////
+	auto& renderContext = g_graphicsEngine->GetRenderContext();
 
-    // ここからゲームループ
-    while (DispatchWindowMessage())
-    {
-        // レンダリング開始
-        g_engine->BeginFrame();
+	// ここからゲームループ
+	while (DispatchWindowMessage())
+	{
+		// レンダリング開始
+		g_engine->BeginFrame();
 
-        //////////////////////////////////////
-        // ここから絵を描くコードを記述する
-        //////////////////////////////////////
+		//////////////////////////////////////
+		// ここから絵を描くコードを記述する
+		//////////////////////////////////////
 
-        // ライトを回す
-        Quaternion qRotY;
-        qRotY.SetRotation(g_vec3AxisY, g_pad[0]->GetLStickXF() * 0.02f);
-        qRotY.Apply(light.dirDirection);
+		// ライトを回す
+		Quaternion qRotY;
+		qRotY.SetRotation(g_vec3AxisY, g_pad[0]->GetLStickXF() * 0.02f);
+		qRotY.Apply(light.dirDirection);
 
-        // 背景モデルをドロー
-        // bgModel.Draw(renderContext);
+		// 背景モデルをドロー
+		// bgModel.Draw(renderContext);
 
-        // ティーポットモデルをドロー
-        teapotModel.Draw(renderContext);
+		// ティーポットモデルをドロー
+		teapotModel.Draw(renderContext);
 
-        //////////////////////////////////////
-        // 絵を描くコードを書くのはここまで！！！
-        //////////////////////////////////////
-        // レンダリング終了
-        g_engine->EndFrame();
-    }
-    return 0;
+		//////////////////////////////////////
+		// 絵を描くコードを書くのはここまで！！！
+		//////////////////////////////////////
+		// レンダリング終了
+		g_engine->EndFrame();
+	}
+	return 0;
 }
 
 /// <summary>
@@ -103,72 +111,72 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 /// <param name="light"></param>
 void InitModel(Model& bgModel, Model& teapotModel, Model& lightModel, Light& light)
 {
-    ModelInitData bgModelInitData;
-    bgModelInitData.m_tkmFilePath = "Assets/modelData/bg.tkm";
+	ModelInitData bgModelInitData;
+	bgModelInitData.m_tkmFilePath = "Assets/modelData/bg.tkm";
 
-    // 使用するシェーダーファイルパスを設定する
-    bgModelInitData.m_fxFilePath = "Assets/shader/sample.fx";
+	// 使用するシェーダーファイルパスを設定する
+	bgModelInitData.m_fxFilePath = "Assets/shader/sample.fx";
 
-    // ディレクションライトの情報を定数バッファーとしてディスクリプタヒープに登録するために
-    // モデルの初期化情報として渡す
-    bgModelInitData.m_expandConstantBuffer = &light;
-    bgModelInitData.m_expandConstantBufferSize = sizeof(light);
+	// ディレクションライトの情報を定数バッファーとしてディスクリプタヒープに登録するために
+	// モデルの初期化情報として渡す
+	bgModelInitData.m_expandConstantBuffer = &light;
+	bgModelInitData.m_expandConstantBufferSize = sizeof(light);
 
-    // 初期化情報を使ってモデルを初期化する
-    bgModel.Init(bgModelInitData);
-    ModelInitData teapotModelInitData;
-    teapotModelInitData.m_tkmFilePath = "Assets/modelData/teapot.tkm";
+	// 初期化情報を使ってモデルを初期化する
+	bgModel.Init(bgModelInitData);
+	ModelInitData teapotModelInitData;
+	teapotModelInitData.m_tkmFilePath = "Assets/modelData/teapot.tkm";
 
-    // 使用するシェーダーファイルパスを設定する
-    teapotModelInitData.m_fxFilePath = "Assets/shader/sample.fx";
+	// 使用するシェーダーファイルパスを設定する
+	teapotModelInitData.m_fxFilePath = "Assets/shader/sample.fx";
 
-    // ディレクションライトの情報を定数バッファーとしてディスクリプタヒープに登録するために
-    // モデルの初期化情報として渡す
-    teapotModelInitData.m_expandConstantBuffer = &light;
-    teapotModelInitData.m_expandConstantBufferSize = sizeof(light);
+	// ディレクションライトの情報を定数バッファーとしてディスクリプタヒープに登録するために
+	// モデルの初期化情報として渡す
+	teapotModelInitData.m_expandConstantBuffer = &light;
+	teapotModelInitData.m_expandConstantBufferSize = sizeof(light);
 
-    // 初期化情報を使ってモデルを初期化する
-    teapotModel.Init(teapotModelInitData);
+	// 初期化情報を使ってモデルを初期化する
+	teapotModel.Init(teapotModelInitData);
 
-    teapotModel.UpdateWorldMatrix(
-        { 0.0f, 20.0f, 0.0f },
-        g_quatIdentity,
-        g_vec3One
-    );
+	teapotModel.UpdateWorldMatrix(
+		{ 0.0f, 20.0f, 0.0f },
+		g_quatIdentity,
+		g_vec3One
+	);
 
-    ModelInitData lightModelInitData;
-    lightModelInitData.m_tkmFilePath = "Assets/modelData/light.tkm";
+	ModelInitData lightModelInitData;
+	lightModelInitData.m_tkmFilePath = "Assets/modelData/light.tkm";
 
-    // 使用するシェーダーファイルパスを設定する
-    lightModelInitData.m_fxFilePath = "Assets/shader/other/light.fx";
-    lightModelInitData.m_expandConstantBuffer = &light;
-    lightModelInitData.m_expandConstantBufferSize = sizeof(light);
+	// 使用するシェーダーファイルパスを設定する
+	lightModelInitData.m_fxFilePath = "Assets/shader/other/light.fx";
+	lightModelInitData.m_expandConstantBuffer = &light;
+	lightModelInitData.m_expandConstantBufferSize = sizeof(light);
 
-    // 初期化情報を使ってモデルを初期化する
-    lightModel.Init(lightModelInitData);
+	// 初期化情報を使ってモデルを初期化する
+	lightModel.Init(lightModelInitData);
 }
 
 void InitDirectionLight(Light& light)
 {
-    // ライトは右側から当たっている
-    light.dirDirection.x = 0.0f;
-    light.dirDirection.y = 0.0f;
-    light.dirDirection.z = 1.0f;
-    light.dirDirection.Normalize();
+	// ライトは右側から当たっている
+	light.dirDirection.x = 0.0f;
+	light.dirDirection.y = 0.0f;
+	light.dirDirection.z = 1.0f;
+	light.dirDirection.Normalize();
 
-    // ライトのカラーは白
-    light.dirColor.x = 0.5f;
-    light.dirColor.y = 0.5f;
-    light.dirColor.z = 0.5f;
+	// ライトのカラーは白
+	light.dirColor.x = 0.5f;
+	light.dirColor.y = 0.5f;
+	light.dirColor.z = 0.5f;
 
-    // 視点の位置を設定する
-    light.eyePos = g_camera3D->GetPosition();
+	// 視点の位置を設定する
+	light.eyePos = g_camera3D->GetPosition();
 }
 
 void InitAmbientLight(Light& light)
 {
-    // アンビエントライト
-    light.ambientLight.x = 0.3f;
-    light.ambientLight.y = 0.3f;
-    light.ambientLight.z = 0.3f;
+	// アンビエントライト
+	light.ambientLight.x = 0.3f;
+	light.ambientLight.y = 0.3f;
+	light.ambientLight.z = 0.3f;
 }
